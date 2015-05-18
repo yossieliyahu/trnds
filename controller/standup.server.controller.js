a718592634eb78cb5f83fbe5cf47a085c6a387f0
yossieliyahu <yossi@montiera.com>
2015-05-18 18:51:59 +0300

    adding the project source files

---
+++ controller/standup.server.controller.js
@@ -0,0 +1,115 @@
+﻿var Standup = require('../models/standup.server.model.js');
+
+/************************************************************************************************
+
+Note that we execute "find", "update" and "remove" on the model.
+The result returned (filtered or not) as a collection of sub documents/fields
+
+ * 
+ * Retreive Documents
+ * 
+ * 
+// #1
+// returns query! not results yet! we need to exec this query against the mongodb
+var q = Standup.find(); 
+
+// #2
+// with callback .. exec the query immediately 
+Standup.find(function (err, results) { 
+
+});
+
+// #3
+// with callback & query conditions .. exec the query immediately 
+Standup.find({"memberName" :" bbb", "project" : { $gt : 10 }}, "memberName workToday",  function (err, results) { 
+
+});
+
+// #4 - also: findOne(), findById()
+
+ * 
+ * Update Documents
+ * 
+* 
+
+// option #1
+var condition = { "memberName" : "bbb" };
+var update = { "impediment" : "some updated value" };
+
+Standup.update(condition, update, { multi : true }, function (err, numberAffected, rawResponse) { });
+
+// option #2
+
+Standup.findOne({ "memberName" : "bbb" }, function (err, doc) {
+    doc.impediment = " some updated value ";
+    doc.save(function (err) { });
+});
+
+
+
+* 
+* Remove Documents
+* 
+* 
+
+Standup.remove() .. 
+
+************************************************************************************************/
+
+exports.list = function (req, res) {
+    var query = Standup.find();
+    
+    query.sort({ createdOn: 'desc' })
+        .limit(12)
+        .exec(function (err, results) {
+        //res.send(results);
+        res.render('index', { title: 'Standup - List', notes: results });
+    });
+};
+
+exports.filterByMember = function (req, res) {
+    var query = Standup.find();
+    var filter = req.body.memberName;
+    
+    query.sort({ createdOn: 'desc' });
+    
+    if (filter.length > 0) {
+        query.where({ memberName: filter })
+    }
+    
+    query.exec(function (err, results) {
+        res.render('index', { title: 'Standup - List', notes: results });
+    });
+};
+
+
+exports.create = function (req, res) {
+    var entry = new Standup({
+        memberName: req.body.memberName,
+        project: req.body.project,
+        workYesterday: req.body.workYesterday,
+        workToday: req.body.workToday,
+        impediment: req.body.impediment
+    });
+    
+    // entry.save();
+    
+    entry.save(function (err) {
+        if (err) {
+            var errMsg = 'Sorry, there was an error saving the stand-up meeting note. ' + err;
+            res.render('newnote', { title: 'Standup - New Note (error)', message: errMsg });
+        }
+        else {
+            console.log('Stand-up meeting note was saved!');
+            // Redirect to the home page to display list of notes...
+            res.redirect(301, '/');
+        }
+    });
+    
+    // Redirect to the home page...
+    // res.redirect(301, '/');
+};
+
+exports.getNote = function (req, res) {
+    res.render('newnote', { title: 'Standup - New Note' });
+};
---
+++ routes/index.js
@@ -0,0 +1,25 @@
+﻿var express = require('express');
+var router = express.Router();
+var standupCtrl = require('../controller/standup.server.controller');
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    // res.render('index', { title: 'Express' });
+    return standupCtrl.list(req, res);
+});
+
+router.post('/', function (req, res) {
+    return standupCtrl.filterByMember(req, res);
+});
+
+/* GET New Note page. */
+router.get('/newnote', function (req, res) {
+    return standupCtrl.getNote(req, res);
+});
+
+/* POST New Note page. */
+router.post('/newnote', function (req, res) {
+    return standupCtrl.create(req, res);
+});
+
+module.exports = router;
